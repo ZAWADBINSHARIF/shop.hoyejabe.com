f77e2a6d8ad9ba98eaf4a15e8168b6471672630b
ZAWADBINSHARIF <syedzawadbinsharif@gmail.com>
2025-06-28 23:59:24 +0600

    [bug fixed]: 1. Removed useless copy right text

--- resources/views/livewire/components/footer.blade.php
+++ resources/views/livewire/components/footer.blade.php
@@ -1,10 +1,10 @@
 <section class="text-gray-700 bg-white body-font" {!! $attributes ?? '' !!}>
     <div class="container flex flex-col items-center px-8 py-8 mx-auto max-w-7xl sm:flex-row">
-        <a href="#_" class="text-xl font-black leading-none text-gray-900 select-none logo">LionBD<span
-                class="text-indigo-600">.</span></a>
-        <p class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">&copy; 2021 Lion BD
-            - MD. Nasif
-        </p>
+        <a href="#_" class="text-xl font-black leading-none text-gray-900 select-none logo">
+            {{ $companyDetails->name ?? 'No Name' }}
+        </a>
+        {{-- <p class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">
+        </p> --}}
 
 
         <span class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
